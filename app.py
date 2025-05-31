b935013a7e1823646dac0f9a45d31d50ef3de133
jtanaka104 <39017713+jtanaka104@users.noreply.github.com>
2025-05-31 17:23:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-
 import streamlit as st
 import openai
-
-# Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
-openai.api_key = st.secrets.OpenAIAPI.openai_api_key
-
+###############################################################################
 # チャットボットとやりとりする関数
+###############################################################################
 def communicate():
     messages = st.session_state["messages"]
 
@@ -22,22 +19,26 @@ def communicate():
     messages.append(bot_message)
 
     st.session_state["user_input"] = ""  # 入力欄を消去
-
+###############################################################################
+# Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
+###############################################################################
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+###############################################################################
 # st.session_stateを使いメッセージのやりとりを保存
+###############################################################################
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
         {"role": "system", "content": st.secrets.AppSettings.system_prompt}
-        ]
-
+    ]
+###############################################################################
 # ユーザーインターフェイスの構築
+###############################################################################
 st.title(" 「プログラミング講師」ボット")
 st.write("プログラミングに関して、何でも聞いてください。")
-
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
-
     for message in reversed(messages[1:]):  # 直近のメッセージを上に
         speaker = "🙂"
         if message["role"]=="assistant":
